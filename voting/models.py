478f67ec45317618a12f8bd4a86ac631a1ec8a00
Luke Pomfrey <luke.pomfrey@titanemail.com>
2015-01-30 16:22:36 +0000

    Upgrade to use swappable AUTH_USER_MODEL

--- voting/models.py
+++ voting/models.py
@@ -2,17 +2,16 @@
 
 from __future__ import unicode_literals
 
-from datetime import datetime
-
-from django.utils.encoding import python_2_unicode_compatible
+from django.conf import settings
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 try:
     from django.utils.timezone import now
 except ImportError:
+    from datetime import datetime
     now = datetime.now
 
 from voting.managers import VoteManager
@@ -23,12 +22,13 @@ SCORES = (
     (-1, '-1'),
 )
 
+
 @python_2_unicode_compatible
 class Vote(models.Model):
     """
     A vote on an object by a User.
     """
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL', 'auth.User'))
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     object = generic.GenericForeignKey('content_type', 'object_id')
