e5b7727ae0a647d70b133044f75f68f54209a541
Luke Pomfrey <luke.pomfrey@titanemail.com>
2015-12-17 09:29:28 +0000

    Update models.py for django 1.9

--- voting/models.py
+++ voting/models.py
@@ -1,26 +1,14 @@
-# coding: utf-8
-
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
-
-try:
-    from django.utils.timezone import now
-except ImportError:
-    from datetime import datetime
-    now = datetime.now
+from django.utils.timezone import now
 
 from voting.managers import VoteManager
-
-
-SCORES = (
-    (+1, '+1'),
-    (-1, '-1'),
-)
 
 
 @python_2_unicode_compatible
@@ -28,10 +16,17 @@ class Vote(models.Model):
     """
     A vote on an object by a User.
     """
+    UPVOTE = +1
+    DOWNVOTE = -1
+    SCORES = (
+        (UPVOTE, '+1'),
+        (DOWNVOTE, '-1'),
+    )
+
     user = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL', 'auth.User'))
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    object = generic.GenericForeignKey('content_type', 'object_id')
+    object = GenericForeignKey('content_type', 'object_id')
     vote = models.SmallIntegerField(choices=SCORES)
     time_stamp = models.DateTimeField(editable=False, default=now)
 
@@ -43,10 +38,10 @@ class Vote(models.Model):
         unique_together = (('user', 'content_type', 'object_id'),)
 
     def __str__(self):
-        return '%s: %s on %s' % (self.user, self.vote, self.object)
+        return '{}: {} on {}'.format(self.user, self.vote, self.object)
 
     def is_upvote(self):
-        return self.vote == 1
+        return self.vote == self.UPVOTE
 
     def is_downvote(self):
-        return self.vote == -1
+        return self.vote == self.DOWNVOTE
