88060ec77616058318cb799fa004fd0dc94b945b
Luke Pomfrey <luke.pomfrey@titanemail.com>
2015-12-17 09:23:41 +0000

    Add migrations

---
+++ voting/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Vote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.PositiveIntegerField()),
+                ('vote', models.SmallIntegerField(choices=[(1, '+1'), (-1, '-1')])),
+                ('time_stamp', models.DateTimeField(default=django.utils.timezone.now, editable=False)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'db_table': 'votes',
+            },
+        ),
+        migrations.AlterUniqueTogether(
+            name='vote',
+            unique_together=set([('user', 'content_type', 'object_id')]),
+        ),
+    ]
